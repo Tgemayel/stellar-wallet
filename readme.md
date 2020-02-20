67a310ca128c25083137b5ea402a6036036d604e
Toni Gemayel <tonigemayel@gmail.com>
2020-02-19 19:22:52 -0800

    stellar wallet

---
+++ readme.md
@@ -0,0 +1,26 @@
+# Stellar Demo Wallet
+
+To start building with this project clone this repo and install the dependecies:
+
+```bash
+npm i
+```
+
+and run:
+
+```bash
+npm start
+```
+
+To build the app for production, run:
+
+```bash
+npm run build
+```
+
+---
+
+### Helpful links:
+- [https://www.stellar.org/developers](https://www.stellar.org/developers)
+- [https://stellar.github.io/js-stellar-sdk/](https://stellar.github.io/js-stellar-sdk/)
+- [https://github.com/stellar/js-stellar-sdk](https://github.com/stellar/js-stellar-sdk)
---
+++ stencil.config.ts
@@ -0,0 +1,43 @@
+import { Config } from '@stencil/core'
+import { sass } from '@stencil/sass'
+import { postcss } from '@stencil/postcss'
+import autoprefixer from 'autoprefixer'
+import nodePolyfills from 'rollup-plugin-node-polyfills'
+
+export const config: Config = {
+  namespace: 'stellar-wallet',
+  devServer: {
+    openBrowser: false
+  },
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader'
+    },
+    {
+      type: 'docs-readme'
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+      baseUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:3333/' : 'https://stellar-demo-wallet.now.sh/'
+    }
+  ],
+  globalStyle: 'src/global/style.scss',
+  commonjs: {
+    namedExports: {
+      'stellar-sdk': ['StrKey', 'xdr', 'Transaction', 'Keypair', 'Networks', 'TransactionBuilder', 'BASE_FEE', 'Operation', 'Asset', 'Memo', 'MemoHash']
+    },
+  },
+  plugins: [
+    nodePolyfills(),
+    sass(),
+    postcss({
+      plugins: [autoprefixer()]
+    })
+  ],
+  nodeResolve: {
+    browser: true,
+    preferBuiltins: true
+  }
+}
